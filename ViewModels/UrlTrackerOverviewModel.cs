5e7d95f24fc1ef5a11658fb194b3d4f9af380df7
AlexanderVdVegt <alexander.van.der.vegt@infocaster.net>
2020-08-27 17:03:50 +0200

    added overviewmodel

---
+++ ViewModels/UrlTrackerOverviewModel.cs
@@ -0,0 +1,15 @@
+﻿using InfoCaster.Umbraco.UrlTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InfoCaster.Umbraco.UrlTracker.ViewModels
+{
+    public class UrlTrackerOverviewModel
+    {
+        public IEnumerable<UrlTrackerModel> Entries { get; set; }
+        public int TotalPages { get; set; }
+        
+    }
+}
