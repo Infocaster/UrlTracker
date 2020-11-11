ff5a2ac1854c1c0c101bad83054b0c2abe8e2dd9
Dinant <73031899+witje@users.noreply.github.com>
2020-11-11 12:56:33 +0100

    Added logging helper

---
+++ Helpers/IUrlTrackerNewLoggingHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace InfoCaster.Umbraco.UrlTracker.Helpers
+{
+	public interface IUrlTrackerNewLoggingHelper
+	{
+		void LogException(Exception ex);
+		void LogInformation(string message, params object[] args);
+		void LogInformation(string message);
+	}
+}
---
+++ Helpers/UrlTrackerNewLoggingHelper.cs
@@ -0,0 +1,36 @@
+﻿using InfoCaster.Umbraco.UrlTracker.Settings;
+using System;
+using Umbraco.Core.Logging;
+
+namespace InfoCaster.Umbraco.UrlTracker.Helpers
+{
+	public class UrlTrackerNewLoggingHelper : IUrlTrackerNewLoggingHelper
+	{
+		private readonly ILogger _logger;
+		private readonly IUrlTrackerNewSettings _urlTrackerSettings;
+
+		public UrlTrackerNewLoggingHelper(
+			ILogger logger,
+			IUrlTrackerNewSettings urlTrackerSettings)
+		{
+			_logger = logger;
+			_urlTrackerSettings = urlTrackerSettings;
+		}
+
+		public void LogException(Exception ex)
+		{
+			_logger.Error(typeof(UrlTrackerComponent), ex);
+		}
+
+		public void LogInformation(string message, params object[] args)
+		{
+			LogInformation(string.Format(message, args));
+		}
+
+		public void LogInformation(string message)
+		{
+			if (_urlTrackerSettings.LoggingEnabled())
+				_logger.Debug(typeof(UrlTrackerComponent), message);
+		}
+    }
+}
