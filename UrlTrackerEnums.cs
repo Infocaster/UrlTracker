8c818f93027125df6daea1d6c8a9806828954a29
Dinant <73031899+witje@users.noreply.github.com>
2020-11-11 12:58:54 +0100

    Added global enums

---
+++ UrlTrackerEnums.cs
@@ -0,0 +1,32 @@
+﻿namespace InfoCaster.Umbraco.UrlTracker
+{
+	public enum UrlTrackerEntryType
+	{
+		Redirect,
+		NotFound
+	}
+
+	public enum UrlTrackerRedirectType
+	{
+		MovedPermanently = 301,
+		TemporaryRedirect = 302
+	}
+
+	public enum UrlTrackerSortType
+	{
+		CreatedDesc,
+		CreatedAsc,
+		OccurencedDesc,
+		OccurencedAsc,
+		LastOccurenceDesc,
+		LastOccurenceAsc
+	}
+
+	public enum UrlTrackerReason
+	{
+		Renamed,
+		Moved,
+		UrlOverwritten,
+		UrlOverwrittenSEOMetadata
+	}
+}
