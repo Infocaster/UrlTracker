940aa79e14e47d804355b54e7b89c29c69c22f88
Dinant <73031899+witje@users.noreply.github.com>
2020-11-12 13:05:28 +0100

    Edited UrlTracker controller

--- Controllers/UrlTrackerManagerController.cs
+++ Controllers/UrlTrackerManagerController.cs
@@ -4,23 +4,24 @@ using InfoCaster.Umbraco.UrlTracker.ViewModels;
 using System.Linq;
 using System.Web.Http;
 using InfoCaster.Umbraco.UrlTracker.NewRepositories;
+using InfoCaster.Umbraco.UrlTracker.Services;
 using Umbraco.Web.WebApi;
 
 namespace InfoCaster.Umbraco.UrlTracker.Controllers
 {
 	public class UrlTrackerManagerController : UmbracoApiController
 	{
-		private readonly IUrlTrackerNewRepository _urlTrackerRepository;
+		private readonly IUrlTrackerService _urlTrackerService;
 
-	    public UrlTrackerManagerController(IUrlTrackerNewRepository urlTrackerRepository)
-	    {
-		    _urlTrackerRepository = urlTrackerRepository;
-	    }
+		public UrlTrackerManagerController(IUrlTrackerService urlTrackerService)
+		{
+			_urlTrackerService = urlTrackerService;
+		}
 
-        [HttpGet]
+		[HttpGet]
         public IHttpActionResult GetRedirects(int skip, int amount)
         {
-	        var entriesResult = _urlTrackerRepository.GetRedirects(skip, amount);
+	        var entriesResult = _urlTrackerService.GetRedirects(skip, amount);
 
             var model = new UrlTrackerOverviewModel
             {
@@ -34,7 +35,7 @@ namespace InfoCaster.Umbraco.UrlTracker.Controllers
         [HttpGet]
         public IHttpActionResult GetNotFounds(int skip, int amount)
         {
-	        var entriesResult = _urlTrackerRepository.GetNotFounds(skip, amount);
+	        var entriesResult = _urlTrackerService.GetNotFounds(skip, amount);
 
 	        var model = new UrlTrackerOverviewModel
 	        {
@@ -46,9 +47,9 @@ namespace InfoCaster.Umbraco.UrlTracker.Controllers
         }
 
         [HttpGet]
-        public IHttpActionResult GetRedirectsByFilter(int skip, int amount, string query)
+        public IHttpActionResult GetRedirectsByFilter(int skip, int amount, string query, UrlTrackerSortType sortType)
         {
-	        var entriesResult = _urlTrackerRepository.GetRedirectsByFilter(skip, amount, searchQuery: query);
+	        var entriesResult = _urlTrackerService.GetRedirectsByFilter(skip, amount, sortType, query);
 
             var model = new UrlTrackerOverviewModel
             {
@@ -62,57 +63,28 @@ namespace InfoCaster.Umbraco.UrlTracker.Controllers
         [HttpGet]
         public IHttpActionResult Details(int id)
         {
-            //var entry = UrlTrackerRepository.GetUrlTrackerEntryById(id);
-            //return Ok(entry);
-
-            return Ok();
+			return Ok(_urlTrackerService.GetEntryById(id));
         }
 
         [HttpPost]
         public IHttpActionResult SaveChanges(UrlTrackerModel model)
         {
-            //try
-            //{
-            //    _urlTrackerRepository.UpdateEntry(model);
-            //}
-            //catch(Exception e)
-            //{
-            //    return BadRequest();
-            //}
-
+            _urlTrackerService.UpdateEntry(model);
             return Ok();
         }
 
-        [HttpPost]
-        public IHttpActionResult Create([FromBody]UrlTrackerModel model)
-        {
-            //try
-            //{
-            //    UrlTrackerRepository.AddUrlTrackerEntry(model);
-            //    return Ok();
-            //}
-            //catch
-            //{
-            //    return BadRequest();
-            //}
+		[HttpPost]
+		public IHttpActionResult AddEntry([FromBody] UrlTrackerModel model)
+		{
+			_urlTrackerService.AddEntry(model);
+			return Ok();
+		}
 
-            return Ok();
-        }
-
-        [HttpPost]
-        public IHttpActionResult Delete(int id)
-        {
-            //try
-            //{
-            //    UrlTrackerRepository.DeleteUrlTrackerEntry(id);
-            //    return Ok();
-            //}
-            //catch
-            //{
-            //    return BadRequest();
-            //}
-
-            return Ok();
-        }
-    }
+		[HttpPost]
+		public IHttpActionResult Delete(int id)
+		{
+			_urlTrackerService.DeleteEntryById(id);
+			return Ok();
+		}
+	}
 }
