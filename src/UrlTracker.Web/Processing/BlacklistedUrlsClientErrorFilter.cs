9e6d9a578f2ea79eb7862bc9445d489b3db91497
Bram van den Bogaard <bram.van.den.bogaard@infocaster.net>
2022-07-07 07:50:34 +0000

    Merged PR 5348: Added filter to block blacklisted items from appsettings
    Also show new setting in backoffice Settings Tree

---
+++ src/UrlTracker.Web/Processing/BlacklistedUrlsClientErrorFilter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using UrlTracker.Core.Configuration.Models;
+using UrlTracker.Web.Events.Models;
+
+namespace UrlTracker.Web.Processing
+{
+    public class BlacklistedUrlsClientErrorFilter : IClientErrorFilter
+    {
+        private readonly UrlTrackerSettings _options;
+
+        public BlacklistedUrlsClientErrorFilter(IOptions<UrlTrackerSettings> options)
+        {
+            _options = options.Value;
+        }
+
+        public ValueTask<bool> EvaluateCandidateAsync(UrlTrackerHandled notification)
+            => new ValueTask<bool>(EvaluateCandidate(notification));
+
+        public bool EvaluateCandidate(UrlTrackerHandled notification)
+        {
+            if (!_options.BlockedUrlsList.Any()) return true;
+            //get the host + path from the url
+            var url = notification.Url.ToString();
+
+            //check if any item in blockedUrl contains the url. The foreach is used instead of a .Contains on the list to make sure that items in the list containing https / querystrings etc also get caught
+            foreach (var blockedUrl in _options.BlockedUrlsList)
+            {
+                if (url.Contains(blockedUrl, StringComparison.InvariantCultureIgnoreCase)) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ test/UrlTracker.Web.Tests/Processing/BlacklistedUrlsClientErrorFilterTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UrlTracker.Core.Database.Models;
+using UrlTracker.Core.Domain.Models;
+using UrlTracker.Resources.Testing;
+using UrlTracker.Resources.Testing.Mocks;
+using UrlTracker.Web.Events.Models;
+using UrlTracker.Web.Processing;
+
+namespace UrlTracker.Web.Tests.Processing
+{
+    public class BlacklistedUrlsClientErrorFilterTests : TestBase
+    {
+        private BlacklistedUrlsClientErrorFilter? _testSubject;
+        
+        public override void SetUp()
+        {
+            _testSubject = new BlacklistedUrlsClientErrorFilter(UrlTrackerSettings!);
+        }
+
+        [TestCase(TestName = "EvaluateCandidateAsync returns true if the incoming request is not blacklisted")]
+        public async Task EvaluateCandidateAsync_ValidCandidate_ReturnsTrue()
+        {
+            // arrange
+            UrlTrackerSettings.Value.BlockedUrlsList = new List<string>()
+            {
+                "item1",
+                "item2",
+                "ipsum"
+            };
+            const string url = "http://example.com/lorem";
+            HttpContextMock = new HttpContextMock(new Uri(url));
+
+            // act
+            bool result = await _testSubject!.EvaluateCandidateAsync(new UrlTrackerHandled(HttpContextMock.Context, Url.Parse(url)));
+
+            // assert
+            Assert.That(result, Is.True);
+        }
+
+        [TestCase(TestName = "EvaluateCandidateAsync returns true if the blacklist is empty")]
+        public async Task EvaluateCandidateAsync_ValidCandidateEmptyBlacklist_ReturnsTrue()
+        {
+            // arrange
+            UrlTrackerSettings.Value.BlockedUrlsList = new List<string>();
+            const string url = "http://example.com/blablabla";
+            HttpContextMock = new HttpContextMock(new Uri(url));
+
+            // act
+            bool result = await _testSubject!.EvaluateCandidateAsync(new UrlTrackerHandled(HttpContextMock.Context, Url.Parse(url)));
+
+            // assert
+            Assert.That(result, Is.True);
+        }
+
+        [TestCase(TestName = "EvaluateCandidateAsync returns false if the incoming request is blacklisted")]
+        public async Task EvaluateCandidateAsync_BlacklistedCandidate_ReturnsFalse()
+        {
+            // arrange
+            UrlTrackerSettings.Value.BlockedUrlsList = new List<string>()
+            {
+                "item1",
+                "item2",
+                "blablabla"
+            };
+            const string url = "http://example.com/blablabla";
+            HttpContextMock = new HttpContextMock(new Uri(url));
+
+            // act
+            bool result = await _testSubject!.EvaluateCandidateAsync(new UrlTrackerHandled(HttpContextMock.Context, Url.Parse(url)));
+
+            // assert
+            Assert.That(result, Is.False);
+        }
+    }
+}
