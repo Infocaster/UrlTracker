80058f68c2196d7fce6ded1e359555dcb889a6c8
Dennis Heutinck <dennis.heutinck@infocaster.net>
2022-08-30 05:44:06 +0000

    Merged PR 5993: Install analyzers for threading and xml documentation
    - Install AsyncFixer
    - Install Microsoft.VisualStudio.Threading.Analyzers
    - Remove duplicate project reference in UrlTracker.Web
    - Enable xml documentation warnings on all projects inside the src folder
    - Add xml documentation to some types

--- src/UrlTracker.Core/Abstractions/IUmbracoContextReferenceAbstraction.cs
+++ src/UrlTracker.Core/Abstractions/IUmbracoContextReferenceAbstraction.cs
@@ -4,13 +4,47 @@ using Umbraco.Cms.Core.Models.PublishedContent;
 
 namespace UrlTracker.Core.Abstractions
 {
+    /// <summary>
+    /// When implemented, this type provides an abstraction layer on top of the <see cref="Umbraco.Cms.Core.UmbracoContextReference"/>
+    /// </summary>
     public interface IUmbracoContextReferenceAbstraction
         : IDisposable
     {
+        /// <summary>
+        /// When implemented, this method finds published content with the given id
+        /// </summary>
+        /// <param name="id">The unique id of the published content</param>
+        /// <returns>The <see cref="IPublishedContent"/> with the given id or <see langword="null"/></returns>
         IPublishedContent? GetContentById(int id);
+
+        /// <summary>
+        /// When implemented, this method produces a media url for given published content
+        /// </summary>
+        /// <param name="content">The content item for which the url should be produced</param>
+        /// <param name="mode">The type of url that should be produced</param>
+        /// <param name="culture">The culture variant for which the url should be produced</param>
+        /// <returns>A <see langword="string" /> that represents the media url</returns>
         string GetMediaUrl(IPublishedContent content, UrlMode mode, string? culture);
+
+        /// <summary>
+        /// When implemented, this method produces a url for given published content
+        /// </summary>
+        /// <param name="content">The content item for which the url should be produced</param>
+        /// <param name="mode">The type of url that should be produced</param>
+        /// <param name="culture">The culture variant for which the url should be produced</param>
+        /// <returns>A <see langword="string" /> that represents the url</returns>
         string GetUrl(IPublishedContent content, UrlMode mode, string? culture);
+
+        /// <summary>
+        /// When implemented, this method returns the response code that Umbraco expects to return in the current request
+        /// </summary>
+        /// <returns>An <see langword="int"/> that represents the response code or <see langword="null"/></returns>
         int? GetResponseCode();
+
+        /// <summary>
+        /// When implemented, this method returns all the content items at the root of the content tree in Umbraco
+        /// </summary>
+        /// <returns>A collection of <see cref="IPublishedContent"/></returns>
         IEnumerable<IPublishedContent> GetContentAtRoot();
     }
 }
