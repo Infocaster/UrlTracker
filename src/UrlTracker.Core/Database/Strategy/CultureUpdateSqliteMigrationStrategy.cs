355da1295fd53a478d3cb5c807d0794b577731ac
Edwin van den Ham <edwin.van.den.ham@infocaster.net>
2023-10-17 06:48:50 +0000

    Merged PR 10524: New culture validation
    Added new culture validation with tests. Added DB migration.
    Issue: https://github.com/Infocaster/UrlTracker/issues/150

---
+++ src/UrlTracker.Core/Database/Strategy/CultureUpdateSqliteMigrationStrategy.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Umbraco.Cms.Infrastructure.Migrations;
+using UrlTracker.Core.Database.Migrations;
+
+namespace UrlTracker.Core.Database.Strategy
+{
+    [ExcludeFromCodeCoverage]
+    public class CultureUpdateSqliteMigrationStrategy : MigrationBase, IMigrationStrategy 
+    {
+        public CultureUpdateSqliteMigrationStrategy(IMigrationContext context) : base(context)
+        {
+        }
+
+        public void DoMigration()
+        {
+            Migrate();  
+        }
+
+        protected override void Migrate()
+        {
+            // No migration needed for Sqlite
+        }
+    }
+}
