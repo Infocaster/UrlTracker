fcdeabc27594a61411c369a93ec4a2edfea84c21
Dennis Heutinck <dennis.heutinck@infocaster.net>
2024-07-26 08:38:24 +0000

    Merged PR 13270: Add migrations to resize redirect source and target values to 2083
    - Changes the existing migration so that the source and target value columns immediately update to the correct value
    - Adds an additional migration to fix any existing installations and extend the column size to it's intended size
    
    Related work items: #52087

---
+++ src/UrlTracker.Core/Database/Migrations/M202407260819_PostMigrationCorrectValueLength.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPoco.DatabaseTypes;
+using Umbraco.Cms.Infrastructure.Migrations;
+using static Umbraco.Cms.Core.Constants.Conventions;
+
+namespace UrlTracker.Core.Database.Migrations
+{
+    [ExcludeFromCodeCoverage]
+    internal class M202407260819_PostMigrationCorrectValueLength
+        : MigrationBase
+    {
+        private const int _urlMaxLength = 2083;
+        private const string _redirectTableName = "urltrackerRedirect";
+        private const string _sourceValueColumn = "sourceValue";
+        private const string _targetValueColumn = "targetValue";
+
+        public M202407260819_PostMigrationCorrectValueLength(IMigrationContext context)
+            : base(context)
+        {
+        }
+
+        protected override void Migrate()
+        {
+            // This migration is specifically for SQL Server, because Sqlite doesn't have explicit sizes
+            if (DatabaseType is SQLiteDatabaseType) return;
+
+            // In a previous version of the URL Tracker, there was an error that caused migrations to fail, because the strategy value columns were shortened
+            // That migration is patched in this version, but additionally, we need to make sure that the columns are corrected for users who used the previous version and might now have the wrong column length
+            Alter.Table(_redirectTableName).AlterColumn(_sourceValueColumn).AsString(_urlMaxLength).NotNullable().Do();
+            Alter.Table(_redirectTableName).AlterColumn(_targetValueColumn).AsString(_urlMaxLength).NotNullable().Do();
+        }
+    }
+}
